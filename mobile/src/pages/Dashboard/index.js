77bab21f6d8d3dbf95c273d1e43d858648d426a4
Dânio Filho <daniofilho@me.com>
2019-09-20 12:34:32 -0300

    bkp

---
+++ mobile/src/pages/Dashboard/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Title, List } from './styles';
+
+import Background from '~/Components/Background';
+import Appointment from '~/Components/Appointment';
+
+const data = [1, 2, 3, 4, 5];
+
+export default function Dashboard() {
+  return (
+    <Background>
+      <Container>
+        <Title>Agendamentos</Title>
+
+        <List
+          data={data}
+          keyExtractor={item => String(item)}
+          renderItem={({ item }) => <Appointment data={item} />}
+        />
+      </Container>
+    </Background>
+  );
+}
+
+Dashboard.navigationOptions = {
+  tabBarLabel: 'Agendamento',
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="event" size={20} color={tintColor} />
+  ),
+};
